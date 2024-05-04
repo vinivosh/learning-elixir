de05c68aee7a511cbae85d2ab7f011fde0a70793
vinivosh <vinicius7427@gmail.com>
2024-05-04 14:35:16 -0300

    Chapter 1 exercises

---
+++ joy-of-elixir/chapter_01.exs
@@ -0,0 +1,14 @@
+#
+# * ############################################################################
+# * Stuff to try out in the REPL ("iex" command)
+# * ############################################################################
+#
+
+# Hello world
+"Hello, World!"
+
+# Numbers of seconds in a day
+24 * 60 * 60
+
+# Average of numbers (a Lost reference... nice. Oldie but goldie)
+(4 + 8 + 15 + 16 + 23 + 42) / 6
