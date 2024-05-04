c741ed83779829acc3bfee9efefbb65249775c80
vinivosh <vinicius7427@gmail.com>
2024-05-04 15:34:55 -0300

    Improve Chapter 5 "average of ages" exercise with pattern matching
    You can use pattern matching in the arguments of a function...
    
    …to "extract" values from maps. Cool feature!

--- joy-of-elixir/chapter_05.exs
+++ joy-of-elixir/chapter_05.exs
@@ -13,9 +13,10 @@ IO.puts(seconds_in_days.(7))
 
 
 # Function to calculate the average between two ages, given inside two maps
-avg_ages = fn (map1, map2) ->
-  (map1.age + map2.age) / 2
-end
+# avg_ages = fn (map1, map2) -> (map1.age + map2.age) / 2 end
+
+# Improvement: using pattern matching in the arguments. Nice feature!
+avg_ages = fn (%{age: age1}, %{age: age2}) -> (age1 + age2) / 2 end
 
 IO.puts(avg_ages.(%{name: "Vini", age: 27}, %{name: "Juliana", age: 23}))
-IO.puts(avg_ages.(%{name: "Nabucodonosor II", age: 54}, %{name: "Xerxes", age: 10/12}))
+IO.puts(avg_ages.(%{name: "Nabucodonosor II", age: 54}, %{name: "Xerxes", age: (10/12)}))
