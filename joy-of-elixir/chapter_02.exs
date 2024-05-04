849979fa229a746355135b19d1a821fafa52277b
vinivosh <vinicius7427@gmail.com>
2024-05-04 14:46:29 -0300

    Chapter 2 exercises

---
+++ joy-of-elixir/chapter_02.exs
@@ -0,0 +1,18 @@
+#
+# * ############################################################################
+# * Stuff to try out in the REPL ("iex" command)
+# * ############################################################################
+#
+
+# Numbers of hours in 30 days, now using variables! 😱😱😱
+seconds = 86400
+seconds * 30 / 60 / 60
+
+# Using variables inside strings
+name = "Vini"
+"sup #{name} 🤓" # <- Huh, elixir deals with unicode shit like emojis just fine. Cool!
+
+# Error demo
+5 / "four"
+# (ArithmeticError) bad argument in arithmetic expression
+# Happens, of course, because we tried to divide a number by a string. Incompatible types, y'know?
