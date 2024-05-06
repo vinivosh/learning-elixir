75679cf0518b77d1fbd724de09cbc6c356d654e3
vinivosh <vinicius7427@gmail.com>
2024-05-06 00:05:19 -0300

    Chapter 8 execises
    Chapter 7 does not have any exercises

---
+++ joy-of-elixir/chapter_08.exs
@@ -0,0 +1,8 @@
+# Testing input and output of strings
+protag = String.trim(IO.gets("Insert the name for the main character in our story: "))
+setting = String.trim(IO.gets("Insert the name for the setting in the story: "))
+macguffin = String.trim(IO.gets("Insert the name for the important object to be sought out in the story: "))
+IO.puts("In that faithful day, #{protag} proceeded into #{setting} with no knowledge that an epic quest for the #{macguffin} would be starting...")
+
+# Testing raw values outside the Elixir REPL ("iex" command)
+# "In that faithful day, #{protag} proceeded into #{setting} with no knowledge that an epic quest for the #{macguffin} would be starting..."
