55421ca43c542cac9be36fc05e0fc201914981d6
vinivosh <vinicius7427@gmail.com>
2024-08-31 16:49:39 -0300

    Implement a router in the People project
    Two new endpoints: /hello/<name> and /goodbye/<name> are now available

---
+++ joy-of-elixir/people/lib/router.ex
@@ -0,0 +1,13 @@
+defmodule People.Router do
+  use Plug.Router
+
+  plug :match
+  plug :dispatch
+
+  get "hello/:name", to: People.Hello
+  get "goodbye/:name", to: People.Goodbye
+
+  match _ do
+    send_resp(conn, 404, "There's nothing here")
+  end
+end
