10371edfc0199d2cc0a4784b784d8a39cd04e428
vinivosh <vinicius7427@gmail.com>
2024-08-31 17:13:58 -0300

    Improve chapter 17 execises
    Now using less variables and more pipe operations!

--- joy-of-elixir/people/lib/get_age.ex
+++ joy-of-elixir/people/lib/get_age.ex
@@ -4,12 +4,13 @@ defmodule People.GetAge do
 
   def init(options), do: options
 
-  def call(%Plug.Conn{params: %{"date" => birthday_str}} = conn, _opts) do
-    {:ok, birthday} = Date.from_iso8601(birthday_str)
-    person = %Person{birthday: birthday}
+  def call(%Plug.Conn{params: %{"date" => birthday}} = conn, _opts) do
+    age =
+      %Person{birthday: Date.from_iso8601!(birthday)}
+      |> Person.age
 
     conn
     |> put_resp_content_type("text/plain")
-    |> send_resp(200, "Your age is #{People.Person.age(person)} years.")
+    |> send_resp(200, "Your age is #{age} years.")
   end
 end
