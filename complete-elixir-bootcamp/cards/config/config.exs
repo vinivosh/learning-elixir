619a3ada2f4c1c7babda036d656735e304490485
vinivosh <36175157+vinivosh@users.noreply.github.com>
2024-10-04 17:42:24 -0300

    Test overriding multiple args in Inspect.inspect/2

--- complete-elixir-bootcamp/cards/config/config.exs
+++ complete-elixir-bootcamp/cards/config/config.exs
@@ -12,7 +12,8 @@ config :cards,
 # import_config "#{config_env()}.exs"
 
 # user = %{name: "Alice", id: "i86a9sdla212", hashed_password: "$2y$10$DyM42fphQJmYa5tCb3wQ2u0lRuwehAU4LbLr7p2DJX8Jk9SWguVbG"}
-list = 1..500 |> Enum.to_list
+# list = 1..500 |> Enum.to_list
+list = [7, 97, 7, 97]
 var_inspected = inspect(list)
 IO.puts("`var_inspected` with default inspect:\n#{var_inspected}\n")
 
@@ -20,7 +21,7 @@ old_inspect_fun = Inspect.Opts.default_inspect_fun()
 
 Inspect.Opts.default_inspect_fun(fn
   value, _ ->
-    old_inspect_fun.(value, Inspect.Opts.new(%{limit: :infinity}))
+    old_inspect_fun.(value, Inspect.Opts.new(limit: 3, charlists: :lists))
 end)
 
 var_inspected = inspect(list)
