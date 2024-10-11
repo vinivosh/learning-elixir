dadfabbca519ad20f053812eea36bdf20a457e39
vinivosh <36175157+vinivosh@users.noreply.github.com>
2024-10-11 15:45:12 -0300

    [Cards] Implement create_hand/1
    This method creates a deck, shuffles it and deals as many cards as
    
    were passed in the `create_hand` variable.

--- complete-elixir-bootcamp/cards/lib/cards.ex
+++ complete-elixir-bootcamp/cards/lib/cards.ex
@@ -60,4 +60,10 @@ defmodule Cards do
       {:error, _reason} -> "File does not exist"
     end
   end
+
+  def create_hand(hand_size) do
+    create_deck()
+    |> shuffle()
+    |> deal(hand_size)
+  end
 end
