ac779a79d7a1180d0d3628c99ed83e9e4ce486dd
vinivosh <36175157+vinivosh@users.noreply.github.com>
2024-10-19 14:45:53 -0300

    [Cards] Implement unit test for shuffle/1
    Start using `import` in cards_test.exs so there's no need to repeat
    
    `Cards.` before every function call.

--- complete-elixir-bootcamp/cards/test/cards_test.exs
+++ complete-elixir-bootcamp/cards/test/cards_test.exs
@@ -6,10 +6,19 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
+  import Cards
+
   describe "create_deck/0" do
     test "should return a list with exactly 52 cards" do
-      deck_length = length(Cards.create_deck())
+      deck_length = length(create_deck())
       assert deck_length == 52
+    end
+  end
+
+  describe "shuffle/1" do
+    test "should randomize a deck" do
+      deck = create_deck()
+      refute deck == shuffle(deck)
     end
   end
 end
