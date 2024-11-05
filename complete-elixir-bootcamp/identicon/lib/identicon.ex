c1ec7c507fc5137be6cdb1877eedec98da8769de
vinivosh <36175157+vinivosh@users.noreply.github.com>
2024-11-05 20:23:50 -0300

    [Identicon] Add simple documentation to the funcs in the module

--- complete-elixir-bootcamp/identicon/lib/identicon.ex
+++ complete-elixir-bootcamp/identicon/lib/identicon.ex
@@ -4,21 +4,29 @@ defmodule Identicon do
   """
 
   @doc """
-  Hello world.
+  Given an `input` string, generates an unique **Identicon** image and saves it
+  to a file
 
   ## Examples
 
-      iex> Identicon.hello()
-      :world
+      iex> Identicon.generate("capivara@gmail.com")
+      "./capivara-at-gmail-dot-com.png"
 
   """
-  def generate_identicon(input) do
+  def generate(input) do
     hash_str(input)
     # |> other_func_1
     # |> other_func_2
     # |> other_func_3
   end
 
+  @doc """
+  Computes the MD5 hash of the `input` string, returning it as a list of numbers
+
+  ## Examples
+      iex> Identicon.hash_str("capivara@gmail.com")
+      []
+  """
   def hash_str(input) do
     input
   end
