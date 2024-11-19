8b92f2a3f0cbfc67a5ed817aba493b033325dff2
vinivosh <36175157+vinivosh@users.noreply.github.com>
2024-11-19 00:15:44 -0300

    [Identicon] Downgrade all necessary dependencies to work with Elixir 1.6

--- complete-elixir-bootcamp/identicon/mix.exs
+++ complete-elixir-bootcamp/identicon/mix.exs
@@ -5,7 +5,7 @@ defmodule Identicon.MixProject do
     [
       app: :identicon,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.6.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,9 +21,15 @@ defmodule Identicon.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
-      {:egd, github: "erlang/egd"}
+      {:nimble_parsec, "~> 0.5.0", runtime: false},
+      {:bunt, "~> 0.2.1", runtime: false},
+      {:earmark, "~> 1.2.0", runtime: false},
+      {:jason, "~> 1.4.4", runtime: false},
+      {:makeup, "~> 1.0.5", runtime: false},
+      {:makeup_elixir, "~> 0.14.1", runtime: false},
+      {:ex_doc, "~> 0.18.0", only: :dev, runtime: false},
+      {:egd, github: "erlang/egd"},
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false}
     ]
   end
 end
