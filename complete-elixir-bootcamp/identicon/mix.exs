8b62944d4247e8a7729a7c469031f2161ace5c19
vinivosh <36175157+vinivosh@users.noreply.github.com>
2024-11-19 00:34:28 -0300

    [Identicon] Better organize dependencies in `mix.exs` file

--- complete-elixir-bootcamp/identicon/mix.exs
+++ complete-elixir-bootcamp/identicon/mix.exs
@@ -21,15 +21,19 @@ defmodule Identicon.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.18.0", only: :dev, runtime: false},
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
+
+      # EGD, which was removed from Erlang a long time ago (at or before OTP 21)
+      {:egd, github: "erlang/egd"},
+
+      # Downgraded packages so the project works properly in Elixir 1.6.x
       {:nimble_parsec, "~> 0.5.0", runtime: false},
       {:bunt, "~> 0.2.1", runtime: false},
       {:earmark, "~> 1.2.0", runtime: false},
       {:jason, "~> 1.4.4", runtime: false},
       {:makeup, "~> 1.0.5", runtime: false},
-      {:makeup_elixir, "~> 0.14.1", runtime: false},
-      {:ex_doc, "~> 0.18.0", only: :dev, runtime: false},
-      {:egd, github: "erlang/egd"},
-      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false}
+      {:makeup_elixir, "~> 0.14.1", runtime: false}
     ]
   end
 end
