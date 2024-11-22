2ebeb364d4dea0105e87d7bedcfcb731ee400b2a
vinivosh <36175157+vinivosh@users.noreply.github.com>
2024-11-22 18:38:27 -0300

    [Discuss] Configure dev.exs to load postgres cred. from env vars

--- complete-elixir-bootcamp/discuss/config/dev.exs
+++ complete-elixir-bootcamp/discuss/config/dev.exs
@@ -33,11 +33,15 @@ config :logger, :console, format: "[$level] $message\n"
 # in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20
 
+env_vars = System.get_env()
+
 # Configure your database
 config :discuss, Discuss.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "discuss_dev",
-  hostname: "localhost",
+
+  hostname: Map.get(env_vars, "POSTGRES_SERVER", "localhost"),
+  port: Map.get(env_vars, "POSTGRES_PORT", "5432"),
+  database: Map.get(env_vars, "POSTGRES_DB", "stephen-grider-discuss"),
+  username: Map.get(env_vars, "POSTGRES_USER", "postgres"),
+  password: Map.get(env_vars, "POSTGRES_PASSWORD", ""),
   pool_size: 10
