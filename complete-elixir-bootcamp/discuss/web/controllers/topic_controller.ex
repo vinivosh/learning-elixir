00ae89967b2c527b8609f49a838a27597af97c0b
vinivosh <36175157+vinivosh@users.noreply.github.com>
2025-01-05 17:16:20 -0300

    [Discuss] Start actually inserting new topics in the DB

--- complete-elixir-bootcamp/discuss/web/controllers/topic_controller.ex
+++ complete-elixir-bootcamp/discuss/web/controllers/topic_controller.ex
@@ -10,8 +10,16 @@ defmodule Discuss.TopicController do
   end
 
   def create(_conn, %{"topic" => topic}) do
-    IO.puts("Topic:\n\n#{inspect(topic, pretty: true)}\n\n")
+    IO.puts("Topic:\n\n#{inspect(topic, pretty: true)}\n")
 
-    # TODO: Render something here, of course!
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, new_topic} ->
+        IO.puts("New topic created:\n\n#{inspect(new_topic, pretty: true)}\n")
+
+      {:error, changeset} ->
+        IO.puts("New topic is invalid! Changeset:\n\n#{inspect(changeset, pretty: true)}\n")
+    end
   end
 end
