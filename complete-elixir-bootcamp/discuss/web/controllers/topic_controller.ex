357065a9815b3bbc5cc272f76045e3b373cf5ede
vinivosh <36175157+vinivosh@users.noreply.github.com>
2025-01-05 17:43:55 -0300

    [Discuss] Change default router to use the TopicController (index func.)

--- complete-elixir-bootcamp/discuss/web/controllers/topic_controller.ex
+++ complete-elixir-bootcamp/discuss/web/controllers/topic_controller.ex
@@ -3,6 +3,10 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
+  def index(conn, _params) do
+    nil
+  end
+
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
