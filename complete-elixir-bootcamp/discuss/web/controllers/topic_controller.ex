8e400a9b050c8df57a2c653e3294fc8e380924db
vinivosh <36175157+vinivosh@users.noreply.github.com>
2025-01-06 18:24:25 -0300

    [Discuss] Implement `delete/1` in TopicController

--- complete-elixir-bootcamp/discuss/web/controllers/topic_controller.ex
+++ complete-elixir-bootcamp/discuss/web/controllers/topic_controller.ex
@@ -55,4 +55,13 @@ defmodule Discuss.TopicController do
         render(conn, "edit.html", changeset: changeset, topic: old_topic)
     end
   end
+
+  def delete(conn, %{"id" => topic_id}) do
+    # credo:disable-for-next-line
+    Repo.get!(Topic, topic_id) |> Repo.delete!()
+
+    conn
+    |> put_flash(:info, "Topic deleted")
+    |> redirect(to: topic_path(conn, :index))
+  end
 end
