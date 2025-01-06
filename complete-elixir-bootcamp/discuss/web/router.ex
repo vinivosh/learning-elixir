d29b5ebebd6da4c85e056921a8dbfb80cfea8440
vinivosh <36175157+vinivosh@users.noreply.github.com>
2025-01-06 18:18:31 -0300

    [Discuss] Change router to start using Phoenix' `resources` helper

--- complete-elixir-bootcamp/discuss/web/router.ex
+++ complete-elixir-bootcamp/discuss/web/router.ex
@@ -17,11 +17,14 @@ defmodule Discuss.Router do
     # Use the default browser stack
     pipe_through(:browser)
 
-    get("/", TopicController, :index)
-    get("/topics/new", TopicController, :new)
-    post("/topics", TopicController, :create)
-    get("/topics/:id/edit", TopicController, :edit)
-    put("/topics/:id", TopicController, :update)
+    # get("/", TopicController, :index)
+    # get("/topics/new", TopicController, :new)
+    # post("/topics", TopicController, :create)
+    # get("/topics/:id/edit", TopicController, :edit)
+    # put("/topics/:id", TopicController, :update)
+    # delete("/topics/:id", TopicController, :delete)
+
+    resources("/", TopicController)
   end
 
   # Other scopes may use custom stacks.
