ca0612fc4f36228304b8e11f0213dcbfd6ba5ff2
vinivosh <36175157+vinivosh@users.noreply.github.com>
2025-01-08 15:34:14 -0300

    [Discuss] WIP - Implement the callback for AuthController

--- complete-elixir-bootcamp/discuss/web/controllers/auth_controller.ex
+++ complete-elixir-bootcamp/discuss/web/controllers/auth_controller.ex
@@ -2,10 +2,34 @@ defmodule Discuss.AuthController do
   use Discuss.Web, :controller
   plug(Ueberauth)
 
-  def callback(conn, params) do
-    IO.puts("\nconn.assigns:\n\n#{inspect(conn.assigns, pretty: true)}")
-    IO.puts("\nparams:\n\n#{inspect(params, pretty: true)}")
+  alias Discuss.User
 
-    nil
+  def callback(
+        %{assigns: %{ueberauth_auth: auth}} = conn,
+        %{"code" => code, "provider" => provider} = params
+      ) do
+    IO.puts("\n`auth` struct:\n\n#{inspect(auth, pretty: true)}")
+    IO.puts("\n`params` struct:\n\n#{inspect(params, pretty: true)}")
+
+    user_params = %{
+      token: auth.credentials.token,
+      email: auth.info.email,
+      provider: provider
+    }
+
+    changeset = User.changeset(%User{}, user_params)
+  end
+
+  # Returns the user with the e-mail specified in the changeset, if found, or
+  # creates and returns a new user if not
+  defp upsert_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        # TODO: Actually create the new user here
+        nil
+
+      user ->
+        {:ok, user}
+    end
   end
 end
