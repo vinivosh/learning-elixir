0072be8921581a3d0de556ca6d514a3b42ef28c8
vinivosh <36175157+vinivosh@users.noreply.github.com>
2025-02-04 20:10:32 -0300

    [Discuss] Create the SetUser plug
    Implement the `init/1` and `call/2` functions properly

--- complete-elixir-bootcamp/discuss/web/controllers/plugs/set_user.ex
+++ complete-elixir-bootcamp/discuss/web/controllers/plugs/set_user.ex
@@ -8,7 +8,6 @@ defmodule Discuss.Plugs.SetUser do
 
   alias Discuss.Repo
   alias Discuss.User
-  alias Discuss.Router.Helpers
 
   @doc """
   Function used in plugs to run some expensive process once, when initializing
@@ -23,6 +22,24 @@ defmodule Discuss.Plugs.SetUser do
   in this plug!
   """
   def call(conn, _params) do
-    nil
+    user_id = get_session(conn, :user_id)
+
+    # credo:disable-for-next-line
+    cond do
+      # This next one-liner condition does three things in total:
+      # - Checks if `user_id` is a truthy value
+      # - Checks if a user is returned successfully by Repo.get (which fetches
+      # a user from our DB)
+      # - It then assigns that user to the `user` var
+      user = user_id && Repo.get(User, user_id) ->
+        # Assigining to the "user" key all info of the `user` found in the DB.
+        # This will be available in `conn["assigns"]["user"]` inside our `conn`
+        # for all plugs used after this one!
+        assign(conn, :user, user)
+
+      # If any of the two booleans in the condition above fail, we fallback here
+      true ->
+        assign(conn, :user, nil)
+    end
   end
 end
