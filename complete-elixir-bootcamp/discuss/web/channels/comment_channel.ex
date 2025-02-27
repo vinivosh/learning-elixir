65ea21829513d47ffa0a0ad22cb07839311cb5a3
vinivosh <36175157+vinivosh@users.noreply.github.com>
2025-02-26 22:55:13 -0300

    [Discuss] Add more useful logs to `channels/comment_channel.ex`

--- complete-elixir-bootcamp/discuss/web/channels/comment_channel.ex
+++ complete-elixir-bootcamp/discuss/web/channels/comment_channel.ex
@@ -6,16 +6,19 @@ defmodule Discuss.CommentChannel do
   use Discuss.Web, :channel
 
   def join(name, _params, socket) do
+    IO.puts("############################################################")
+    IO.puts("Websockets are working! Channel was joined with success.")
+    IO.puts("Name:\n#{inspect(name, limit: :infinity)}")
+    IO.puts("############################################################")
     {:ok, %{}, socket}
   end
 
   def handle_in(name, msg, socket) do
     IO.puts("########################################")
-    IO.puts("Name:")
-    IO.puts(name)
+    IO.puts("Websockets are working! Message was received with success.")
+    IO.puts("Name:\n#{inspect(name, limit: :infinity)}")
     IO.puts("Message:\n#{inspect(msg, limit: :infinity)}")
     IO.puts("########################################")
-
     {:reply, :ok, socket}
   end
 end
