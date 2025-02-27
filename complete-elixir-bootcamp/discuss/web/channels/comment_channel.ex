d296affd6b17106389e17aabecb2d90dfba7b788
vinivosh <36175157+vinivosh@users.noreply.github.com>
2025-02-26 23:36:40 -0300

    [Discuss] Implement pulling out the comment added in CommentChannel
    Also pull out the relevant topic ID

--- complete-elixir-bootcamp/discuss/web/channels/comment_channel.ex
+++ complete-elixir-bootcamp/discuss/web/channels/comment_channel.ex
@@ -5,20 +5,27 @@ defmodule Discuss.CommentChannel do
 
   use Discuss.Web, :channel
 
-  def join(name, _params, socket) do
+  alias Discuss.Topic
+
+  def join("comment:" <> topic_id = name, _params, socket) do
     IO.puts("############################################################")
     IO.puts("Websockets are working! Channel was joined with success.")
     IO.puts("Name:\n#{inspect(name, limit: :infinity)}")
     IO.puts("############################################################")
+
+    topic_id = String.to_integer(topic_id)
+    _topic = Repo.get(Topic, topic_id)
+
     {:ok, %{}, socket}
   end
 
-  def handle_in(name, msg, socket) do
+  def handle_in(name, %{"content" => _content} = msg, socket) do
     IO.puts("########################################")
     IO.puts("Websockets are working! Message was received with success.")
-    IO.puts("Name:\n#{inspect(name, limit: :infinity)}")
-    IO.puts("Message:\n#{inspect(msg, limit: :infinity)}")
+    IO.puts("Name: #{inspect(name, limit: :infinity)}")
+    IO.puts("Message: #{inspect(msg, limit: :infinity)}")
     IO.puts("########################################")
+
     {:reply, :ok, socket}
   end
 end
