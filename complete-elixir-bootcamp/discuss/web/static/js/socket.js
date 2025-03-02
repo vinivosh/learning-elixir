1035fcad9e914d44a5cc4c2c2258e39e70800d49
vinivosh <36175157+vinivosh@users.noreply.github.com>
2025-03-02 12:20:57 -0300

    [Discuss] Start actually rendering comments in each topic's page

--- complete-elixir-bootcamp/discuss/web/static/js/socket.js
+++ complete-elixir-bootcamp/discuss/web/static/js/socket.js
@@ -7,13 +7,29 @@ socket.connect()
 const createSocket = (topicID) => {
   let channel = socket.channel(`comment:${topicID}`, {})
   channel.join()
-    .receive("ok", resp => { console.log("Joined successfully", resp) })
+    .receive("ok", resp => {
+        console.log(`Joined successfully! Resp received, with ${resp.comments.length} comments`)
+        return renderComments(resp.comments) 
+      }
+    )
     .receive("error", resp => { console.log("Unable to join", resp) })
 
     document.querySelector('button').addEventListener("click", () => {
       const content = document.querySelector('textarea').value
       channel.push('commend:add', { content: content })
     })
+}
+
+function renderComments(comments) {
+  const renderedComments = comments.map(comment => {
+    return `
+      <li class="collection-item">
+        ${comment.content}
+      </li>
+    `
+  })
+
+  document.querySelector(".collection").innerHTML = renderedComments.join('')
 }
 
 window.createSocket = createSocket
